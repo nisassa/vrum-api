95d5ef8353b3c8d18dfb9fd62e2f8cf1285bdf1a
nisassa <dyma131993@gmail.com>
2022-11-26 00:31:40 +0000

    fix upload

--- app/Http/Requests/User/UpdateUserRequest.php
+++ app/Http/Requests/User/UpdateUserRequest.php
@@ -26,16 +26,17 @@ class UpdateUserRequest extends FormRequest
         return [
             'first_name' => 'required|string|max:255',
             'last_name' => 'required|string|max:255',
-            'postcode' => 'required|string|max:50',
-            'line_1' => 'required|string|max:255',
+            'postcode' => 'nullable|string|max:50',
+            'line_1' => 'nullable|string|max:255',
             'line_2' => 'nullable|string|max:255',
-            'city' => 'required|string|max:255',
+            'city' => 'nullable|string|max:255',
             'county' => 'nullable|string|max:255',
             'country' => 'required|string|max:2',
             'phone' => 'required|string|max:255',
             'password' => 'required|confirmed|string|min:5|max:255',
             'job_title' => 'nullable|string|max:255',
             'landline' => 'nullable|string|max:255',
+            'discard' => 'nullable|number|max:255',
             'photo' => 'nullable|string|max:525',
         ];
     }
